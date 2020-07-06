4c2497ab20581f3fbcfbfa0809a910e248425741
Guillermo Wenrich <gwenrich@google.com>
2020-07-06 18:51:37 -0400

    Fixed content script indenting

--- src/assets/content.js
+++ src/assets/content.js
@@ -1,10 +1,9 @@
 /// <reference types="chrome"/>
 
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.message === 'send links') {
-            sendResponse(Array.from(document.links).map(function(val) {
-                return val.href;
-            }));
-        }
-    });
+chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
+  if (request.message === 'send links') {
+    sendResponse(Array.from(document.links).map(function(val) {
+      return val.href;
+    }));
+  }
+});
