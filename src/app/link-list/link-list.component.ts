5d515435863795cf71a54b6178926130b0d1b83c
Guillermo Wenrich <gwenrich@google.com>
2020-07-06 13:47:26 -0400

    Added search bar that filters result by the inputed regular expression

--- src/app/link-list/link-list.component.ts
+++ src/app/link-list/link-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { LinkService } from '../link.service';
 
+import { Observable, Subject} from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
 @Component({
   selector: 'app-link-list',
   templateUrl: './link-list.component.html',
@@ -10,9 +13,18 @@ export class LinkListComponent implements OnInit {
 
   constructor(private linkService: LinkService) { }
 
-  public links: string[];
+  links$: Observable<string[]>;
+  private searchTerms = new Subject();
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
 
   ngOnInit(): void {
-    this.links = this.linkService.getLinks();
+    this.links$ = this.searchTerms.pipe(
+        distinctUntilChanged(),
+        debounceTime(300),
+        switchMap((term: string) => this.linkService.filterLinks(term))
+    );
   }
 }
--- src/app/link.service.ts
+++ src/app/link.service.ts
@@ -1,6 +1,8 @@
 /// <reference types="chrome"/>
 import { Injectable, ApplicationRef } from '@angular/core';
 
+import { Observable, of} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -18,22 +20,31 @@ export class LinkService {
     }
   }
 
+  filterLinks(term: string): Observable<string[]> {
+    if (!term.trim()) {
+      return of(this.linkList);
+    }
+    const regExp = new RegExp(term);
+    return of(this.linkList.filter((str) => regExp.test(str)));
+  }
+
   constructor(private applicationRef: ApplicationRef) {
 
     chrome.tabs.query({
       active: true,
-      currentWindow: true},
-      (tabs: any) => {
-        console.log('Sending request for links');
-        chrome.tabs.sendMessage(tabs[0].id, {
-            message: 'send links'
-          }, (links: string[]) => {
-            console.log(links);
-            this.addLinks(links);
-            console.log('Links added');
-            this.applicationRef.tick();
-          });
+      currentWindow: true
+    },
+    (tabs: any) => {
+      console.log('Sending request for links');
+      chrome.tabs.sendMessage(tabs[0].id, {
+        message: 'send links'
+      }, (links: string[]) => {
+        console.log(links);
+        this.addLinks(links);
+        console.log('Links added');
+        this.applicationRef.tick();
       });
+    });
   }
 
 }
