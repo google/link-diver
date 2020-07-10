fb032875ee6cfa3a6d36e21c410b7b8f44880c5c
Guillermo Wenrich <gwenrich@google.com>
2020-07-10 08:47:52 -0400

    Removed old file that slipped through the cracks

--- src/app/red-match.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightMatchPipe } from './highlight-match.pipe';
-
-describe('highlightMatchPipe', () => {
-  it('create an instance', () => {
-    const pipe = new HighlightMatchPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
