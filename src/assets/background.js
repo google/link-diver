f4b64d40caec9f63ea31d5ab64f13bbc6556396c
Guillermo Wenrich <gwenrich@google.com>
2020-07-13 10:17:20 -0400

    Added item in context menu to launch app

--- src/assets/background.js
+++ src/assets/background.js
@@ -1,12 +1,27 @@
 // eslint-disable-next-line spaced-comment
 /// <reference types="chrome"/>
 
-// Listens for a click of the extension button at which point it launches
-// the extension in a new tab
-chrome.browserAction.onClicked.addListener(function(activeTab) {
+function launchApp(activeTab) {
   chrome.tabs.create({
     url: chrome.runtime.getURL('index.html'),
     openerTabId: activeTab.id,
     index: activeTab.index + 1
   });
+}
+
+// Listens for a click of the extension button at which point it launches
+// the extension in a new tab
+chrome.browserAction.onClicked.addListener(launchApp);
+
+// Creates an item in the context menu which allows the user to launch the app
+chrome.contextMenus.create({
+  title: 'Link Diver',
+  onclick: function(e) {
+    chrome.tabs.query({
+      active: true,
+      currentWindow: true
+    }, function(tabs) {
+      launchApp(tabs[0]);
+    });
+  }
 });
