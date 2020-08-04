202ec550308c8d258b016bce2491eca18e28585f
Guillermo Wenrich <gwenrich@google.com>
2020-08-04 17:09:40 -0400

    Removed import

--- src/app/fetch-status.service.ts
+++ src/app/fetch-status.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, EventEmitter } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { HttpClient, HttpResponseBase } from '@angular/common/http';
 
 import { LinkData } from './link.service';
