04be7d593a7680bdf7c0789dc093c39a0990ded1
Guillermo Wenrich <gwenrich@google.com>
2020-08-11 19:42:05 -0400

    Added comments to interfaces

--- src/app/interfaces.ts
+++ src/app/interfaces.ts
@@ -15,6 +15,9 @@ export interface LinkData {
     contentType?: string;
 }
 
+/**
+ * Packages all of the data needed to properly display a group of links
+ */
 export interface GroupData {
     key: string,
     list: LinkData[],
@@ -22,11 +25,18 @@ export interface GroupData {
     hide?: boolean
 }
 
+/**
+ * Stores the number of groups and number of links found in a query so that it
+ * can be displayed by the GroupCountComponent.
+ */
 export interface GroupCount {
     numGroups: number;
     numLinks: number;
 }
 
+/**
+ * Enumerates the different ways to sort the list of links.
+ */
 export enum SortOptions {
     DOM,
     DOMReverse,
